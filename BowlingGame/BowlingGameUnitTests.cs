aca6edc3f0ff3ce37f5c6975f56f211300ec506f
fbriers <fbriers@west.com>
2019-09-05 13:03:42 +0100

    Add project files.

---
+++ BowlingGame/BowlingGameUnitTests.cs
@@ -0,0 +1,101 @@
+﻿using System.Reflection.Metadata;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace BowlingGame
+{
+
+    [TestFixture]
+    public class BowlingGameUnitTests
+    {
+        private Program.BowlingGame _bg;
+
+        private void RollMany(int rolls, int pins)
+        {
+            for (int i = 0; i < rolls; i++)
+            {
+                _bg.Roll(pins);
+            }
+        }
+
+        private void RollSpare()
+        {
+            _bg.Roll(5);
+            _bg.Roll(5);
+        }
+
+        private void RollStrike()
+        {
+            _bg.Roll(10);
+        }
+
+        [SetUp]
+        public void NewGame()
+        {
+            _bg = new Program.BowlingGame();
+            //_bg = null;
+        }
+
+        [Test]
+        public void GivenARollThenScore()
+        {
+            _bg.Roll(0);
+            Assert.AreEqual(expected:0,actual: 0);
+        }
+
+        [Test]
+        public void GivenAGutterBallThenStartScoring()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                _bg.Roll(0);
+            }
+            
+            Assert.AreEqual(0, _bg.Score());
+        }
+
+        [Test]
+        public void GivenAllGutterBallsThen0()
+        {
+            RollMany(20,0);
+            Assert.AreEqual(0,_bg.Score());
+        }
+
+        [Test]
+        public void GivenAll1SThen20()
+        {
+            RollMany(20,1);
+            Assert.AreEqual(20,_bg.Score());
+        }
+
+        [Test]
+        public void Given1SpareAnd19GutterBallsThenScoreEquals16()
+        {
+            RollSpare();
+            _bg.Roll(pins:3);
+            RollMany(17,0);
+            Assert.AreEqual(16, _bg.Score());
+        }
+
+        [Test]
+        public void GivenOneStrikeThenScoreEquals24()
+        {
+            RollStrike();
+            _bg.Roll(3);
+            _bg.Roll(4);
+
+            RollMany(16, 0);
+            Assert.AreEqual(24, _bg.Score());
+
+        }
+
+        [Test]
+        public void Given12StrikeThenScoreEquals300()
+        {
+            RollMany(12,10);
+            Assert.AreEqual(300, _bg.Score());
+        }
+
+
+    }
+}
---
+++ BowlingGame/Program.cs
@@ -0,0 +1,77 @@
+﻿ using System;
+
+namespace BowlingGame
+{
+    class Program
+    {
+        static void Mains(string[] args)
+        {
+           var bowlingGame = new BowlingGame();
+        }
+
+        public class BowlingGame
+        {
+            int[] _rolls = new int[21];
+            private int _currentRoll = 0;
+
+            public void Roll(int pins)
+            {
+                _rolls[_currentRoll++] = pins;
+                //_score += pins;
+            }
+
+            public int Score()
+            {
+                int score = 0;
+                int firstInFrame = 0;
+                for (int frame = 0; frame <10; frame++)
+                {
+                    if (IsStrike(firstInFrame))
+                    {
+                        score += 10 + NextTwoBallsAfterStrike(firstInFrame);
+                        firstInFrame++;
+                    }
+
+                    else if (IsSpare(firstInFrame))
+                    {
+                        score += 10 + NextBallAfterSpare(firstInFrame);
+                        firstInFrame += 2;
+                    }
+                    else
+                    {
+                        score += TwoBallsInFrame(firstInFrame);
+                        firstInFrame += 2;
+                    }
+                   
+                }
+                return score;
+            }
+
+            private int TwoBallsInFrame(int firstInFrame)
+            {
+                return _rolls[firstInFrame] + _rolls[firstInFrame + 1];
+            }
+
+            private int NextBallAfterSpare(int firstInFrame)
+            {
+                return _rolls[firstInFrame + 2];
+            }
+
+            private bool IsStrike(int firstInFrame)
+            {
+                return _rolls[firstInFrame] == 10;
+            }
+
+            private bool IsSpare(int firstInFrame)
+            {
+                return _rolls[firstInFrame] + _rolls[firstInFrame + 1] == 10;
+            }
+
+            private int NextTwoBallsAfterStrike(int firstInFrame)
+            {
+                return _rolls[firstInFrame + 1] + _rolls[firstInFrame + 2];
+            }
+        }
+
+    }
+}
